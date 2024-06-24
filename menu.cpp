42f6b2094318c08a9db98022dd5509f6e441c0b0
femamanic <femamanic@unjbg.edu.pe>
2024-06-24 08:48:18 -0500

    Agregar menu.cpp : opción depositar, retirar, mostrar saldo y do_while

--- menu.cpp
+++ menu.cpp
@@ -1,18 +1,59 @@
 #include <iostream>
 using namespace std;
-int main () {
-    system ("cls");
+
+void depositar(float);
+void retirar(float);
+
+float saldo = 0; // Variable global - cualquier función puede acceder a ella
+
+int main(){
+    float dep, ret;
     int op;
-    cout << "CAJERO AUTOMATICO\n";
-    cout << "[1] Depositar\n";
-    cout << "[2] Retirar\n";
-    cout << "[3] Mostrar saldo\n";
-    cout << "[0] Salir\n";
-    cout << "Seleccione una opcion: "; cin >> op;
-    switch (op) {
-        case 0 :
-        cout << "Saliendo ...\n";
-        system ("pause>nul");
-        break;
-    }
+    do {
+        system("cls");
+        cout << "CAJERO AUTOMATICO\n";
+        cout << "[1] Depositar\n";
+        cout << "[2] Retirar\n";
+        cout << "[3] Mostrar saldo\n";
+        cout << "[0] Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> op;
+        switch (op){
+        case 1:
+            cout << "Ingrese el monto a depositar: ";
+            cin >> dep;
+            depositar(dep);
+            cout << "Deposito exitoso\n";
+            system("pause>nul");
+            break;
+        case 2:
+            cout << "Ingrese el monto a retirar: ";
+            cin >> ret;
+            retirar(ret);
+            cout << "Retiro exitoso\n";
+            system("pause>nul");
+            break;
+        case 3:
+            cout << "Mostrando saldo ...\n"
+                << saldo << "\n";
+            system("pause>nul");
+            break;
+        case 0:
+            cout << "Saliendo ...\n";
+            system("pause>nul");
+            break;
+        default :
+            cout << "Opcion no valida\n";
+            system("pause>nul");
+            break;
+        }
+    } while (op != 0);
+}
+
+void depositar(float monto){
+    saldo = saldo + monto;
+}
+
+void retirar(float monto){
+    saldo = saldo - monto;
 }
