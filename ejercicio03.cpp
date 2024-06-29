660315edb0b86bd46613c6382537c25cd6c05599
femamanic <femamanic@unjbg.edu.pe>
2024-06-29 17:32:44 -0500

    Actualizar ejercicio03.cpp : opción volver a ejecutar, error menor e igual a 0

--- ejercicio03.cpp
+++ ejercicio03.cpp
@@ -5,12 +5,30 @@ using namespace std;
 void hipotenusa(float, float);
 
 int main () {
-    float a, b;
-    system("cls");
-    cout << "Ingrese el valor del cateto a: "; cin >> a;
-    cout << "Ingrese el valor del cateto b: "; cin >> b;
-    hipotenusa(a, b);
-    system("pause>nul");
+    float a, b, op;
+    do {
+        system("cls");
+        cout << "CALCULAR LA HIPOTENUSA DE UN TRIANGULO RECTANGULO\n";
+        cout << "-------------------------------------------------\n";
+        cout << "Ingrese el valor del cateto a: "; cin >> a;
+        cout << "Ingrese el valor del cateto b: "; cin >> b;
+        if (a <= 0 || b <= 0) {
+            cout << "-------------------------------------------------\n";
+            cout << "Los valores de los catetos deben ser mayores a 0\n";
+            system("pause>nul");
+            system("cls");
+        }
+        else {
+            cout << "-------------------------------------------------\n";
+            hipotenusa(a, b);
+            system("pause>nul");
+            system("cls");
+        }
+        cout << "Desea calcular otra hipotenusa?\n[1] Si\n[2] No\n"; cin >> op;
+    } while (op == 1);
+    if (op != 1) {
+        cout << "Gracias por usar el programa.\n\nSaliendo...\n";
+    }
     return 0;
 }
 
