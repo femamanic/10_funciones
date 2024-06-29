659680a913824174d5a06e25ad26b0a7fcbd21b6
femamanic <femamanic@unjbg.edu.pe>
2024-06-29 11:51:59 -0500

    Agregar main.cpp : Programa principal

---
+++ Proyecto/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "funciones.h"
+
+using namespace std;
+
+int main () {
+    int a = 10, b = 5;
+    cout << "Suma: " << suma(a, b) << "\n";
+    cout << "Resta: " << resta(a, b) << "\n";
+    return 0;
+}
