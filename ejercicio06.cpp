c899bb6923cd057a61c6ea9b02004e8bd9b35045
femamanic <femamanic@unjbg.edu.pe>
2024-07-01 07:10:57 -0500

    Actualizar ejercicio06.cpp : opción volver a jugar, error usuario elije 'opción invalida'

--- ejercicio06.cpp
+++ ejercicio06.cpp
@@ -9,28 +9,54 @@ int hum = 0; //variable global, humano gana
 int com = 0; //variable global, computadora gana
 
 int main () {
-    int a;
+    int a, op;
     srand(time(0));
     do {
+        hum = 0;
+        com = 0;
+        do {
+            system("cls");
+            cout << "  PIEDRA, PAPEL O TIJERA\n";
+            cout << "Primero en llegar a 3 gana\n";
+            cout << "--------------------------\n";
+            cout << "\tCONTADOR:\n";
+            cout << "Humano: " << hum << "\n";
+            cout << "Computadora: " << com << "\n";
+            cout << "--------------------------\n";
+            if (hum < 3 && com < 3) {
+                cout << "[1] Piedra\n[2] Papel\n[3] Tijera\n";
+                cout << "--------------------------\n";
+                cout << "Ingrese su opcion: "; cin >> a;
+                jugar(a);
+                system("pause>nul");
+            }
+        } while (hum < 3 && com < 3);
         system("cls");
-        cout << "CONTADOR:\n";
-        cout << "Humano: " << hum << "\n";
-        cout << "Computadora: " << com << "\n";
-        cout << "[1] Piedra\n[2] Papel\n[3] Tijera\n";
-        cout << "Ingrese su opcion: "; cin >> a;
-        jugar(a);
+        if (hum == 3) {
+            cout << "Ganaste.\n\nFelicidades...\n";
+        }
+        else {
+            cout << "Perdiste.\n\nLa proxima sera...\n";
+        }
         system("pause>nul");
-    } while (hum < 3 && com < 3);
+        system("cls");
+        cout << "Desea jugar de nuevo?\n[1] Si\n[2] No\n"; cin >> op;
+    } while (op == 1);
+    if (op != 1) {
+        system("cls");
+        cout << "Gracias por jugar.\n\nSaliendo...\n";
+        system("pause>nul");
+    }
     return 0;
 }
 
 void jugar (int a) {
-    int b = rand() % 3;
+    int b = rand() % 3 + 1;
     if (a == 1) { //piedra
-        if (b == 0) { //piedra
+        if (b == 1) { //piedra
             cout << "Empate\n";
         }
-        else if (b == 1) { //papel
+        else if (b == 2) { //papel
             cout << "Computadora gana\n";
             com++;
         }
@@ -40,11 +66,11 @@ void jugar (int a) {
         }
     }
     else if (a == 2) { //papel
-        if (b == 0) { //piedra
+        if (b == 1) { //piedra
             cout << "Humano gana\n";
             hum++;
         }
-        else if (b == 1) {  //papel
+        else if (b == 2) {  //papel
             cout << "Empate\n";
         }
         else { //tijera
@@ -52,17 +78,20 @@ void jugar (int a) {
             com++;
         }
     }
-    else { //tijera
-        if (b == 0) { //piedra
+    else if (a == 3){ //tijera
+        if (b == 1) { //piedra
             cout << "Computadora gana\n";
             com++;
         }
-        else if (b == 1) { //papel
+        else if (b == 2) { //papel
             cout << "Humano gana\n";
-            hum++;
+            hum++;  
         }
         else { //tijera
             cout << "Empate\n";
         }
+    }
+    else {
+        cout << "Opcion invalida\n";
     }
 }
